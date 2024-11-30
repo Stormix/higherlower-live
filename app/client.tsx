7f651bba05ad4c0ef86df64d321e064e7e7d86c1
Anas Mazouni <madadj4@gmail.com>
2024-11-30 12:47:29 +0100

    fix: remove react-scan

--- app/client.tsx
+++ app/client.tsx
@@ -1,11 +1,7 @@
 import { StartClient } from "@tanstack/start";
 import { hydrateRoot } from "react-dom/client";
 /// <reference types="vinxi/types/client" />
-import { scan } from "react-scan";
 import { createRouter } from "./router";
 const router = createRouter();
-scan({
-  enabled: true,
-  log: true, // logs render info to console (default: false)
-});
+
 hydrateRoot(document, <StartClient router={router} />);
